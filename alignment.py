ff82f1672a4470e56f79d9162384336292f4bb81
Kenobi98 <majilakartik123@gmail.com>
2022-10-12 15:45:24 +0530

    Script to map paralogs

---
+++ alignment.py
@@ -0,0 +1,204 @@
+import numpy as np
+import pandas as pd
+
+import Bio
+from Bio import SeqIO
+from Bio.Emboss.Applications import NeedleCommandline
+from Bio import AlignIO
+
+
+
+class Align_Paralogs():
+	def __init__(self):
+		self.hdac = "HDAC1"
+		self.exclude = True    # If True, excludes the other paralog.
+		self.remove = "HDAC1" if self.hdac == "HDAC2" else "HDAC2"
+		self.data = pd.read_csv("NuRD Complex Interactions.xlsx - NuRD Member XL Sites.csv")
+		self.fasta_path = "./Uniprot/"
+		self.output_path = "./EMBOSS/Needle/"
+		self.outputfile_name = "Mapped_XLs_HDAC1"
+		self.included_proteins = ["CHD", "GATA", "HDAC", "MBD", "MTA", "RBBP"]
+		self.mapping = {i:[] for i in ["Protein 1", "Residue 1", "Protein 2", "Residue 2"]}
+
+
+	def forward( self ):
+		print("No. of XLs for each protein = ", self.data["Protein 1"].value_counts() )
+
+		alignments_dict = self.map_aligned_pos(  )
+		
+		# exit()
+		self.mapped_XLs( alignments_dict )
+		df = pd.DataFrame()
+		for key in self.mapping.keys():
+			df[key] = self.mapping[key]
+			print( key, " --> ", len( df[key] ) )
+
+		df.to_csv( f"{self.outputfile_name}.csv", index = False )
+
+
+	def get_aligned_pos(self, target, query, position ):
+		# Return the residue position from the aligned seq for protein2.
+		# target --> sequence for which the aligned position is required (dominant paralog).
+		# query --> sequence to which target is mapped (non-dominant paralog).
+		# position --> residue position of query seq for which the aligned target residue position is required.
+
+		iter1 = 1
+		for i, pos in enumerate( query ):
+			if pos != "-":
+				if iter1 == position:
+					idx = i
+					break
+				iter1 += 1
+		
+		iter2 = 1
+		for j in range( len( target[:idx] ) ):
+			if target[idx] == "-":
+				return 0
+				break
+			elif target[j] != "-":
+				iter2 += 1
+			else:
+				continue
+		# print(idx, "  ", iter1, "  ", iter2, "  ", position)
+		return iter2
+
+	def Needle_alignment(self, seqa_path, seqb_path, outputfile ):
+		# Sequence alignment using EMBOSS Needle.
+		# seqa_path --> PATH for seqa fatsa file.
+		# seqb_path --> PATH for seqb fatsa file.
+		# outputfile --> Output file PATH.
+
+		# Read the seq from fasra file.
+		for record in SeqIO.parse( seqa_path, "fasta"):
+			seqa = record.seq
+		for record in SeqIO.parse( seqb_path, "fasta"):
+			seqb = record.seq
+		
+		# Define parameters for the alignment.
+		needle_cline = NeedleCommandline()
+		needle_cline.asequence = "asis:" + seqa
+		needle_cline.bsequence="asis:"+seqb
+		needle_cline.gapopen=10
+		needle_cline.gapextend=0.5
+		needle_cline.endopen=10
+		needle_cline.endextend=0.5
+		needle_cline.outfile= outputfile
+
+		stdout, stderr = needle_cline()
+		alignments = AlignIO.read(outputfile, "emboss")
+
+		# if "HDAC" in seqa_path:
+		# 	print("".join([a for a in alignments[0]] )[89])
+		# 	print("".join([a for a in alignments[1]] )[89])
+
+		return "".join([a for a in alignments[0]] ), "".join([b for b in alignments[1]] )
+
+
+	def get_paralog(self, protein ):
+		# Returns all the paralogs of a protein.
+		if protein == "CHD":
+			return ["CHD4", "CHD3"]
+		if protein == "GATA":
+			return ["GATAD2B", "GATAD2A"]
+		if protein == "HDAC":
+			return ["HDAC1", "HDAC2"]
+		if protein == "MBD":
+			return ["MBD3", "MBD2"]
+		if protein == "MTA":
+			return ["MTA1", "MTA2", "MTA3"]
+		if protein == "RBBP":
+			return ["RBBP7", "RBBP4"]
+		else:
+			print("Please enter the correct protein name...")
+			exit()
+		
+
+	def return_dominant_paralog(self, prt ):
+		# Returns the dominant paralog for a protein.
+		if prt in ["CHD", "CHD3", "CHD4"]:
+			return "CHD4"
+		elif prt in ["GATA", "GATAD2A", "GATAD2B"]:
+			return "GATAD2B"
+		elif prt in ["HDAC1", "HDAC2"]:
+			return "HDAC1"
+		elif prt in ["MBD", "MBD2", "MBD3"]:
+			return "MBD3"
+		elif prt in ["MTA", "MTA1", "MTA2", "MTA3"]:
+			return "MTA1"
+		elif prt in ["RBBP", "RBBP4", "RBBP7"]:
+			return "RBBP7"
+		else:
+			print(" --> ", prt)
+			print("Please enter the correct protein name...")
+			exit()
+
+
+	def map_aligned_pos( self ):
+		# Align the protein sequences and map the aligned positions to dominant isoform.
+		alignments_dict = {}
+
+		for protein in self.included_proteins:
+			# Get all the paralogs for the protein.
+			all_proteins = self.get_paralog( protein )
+			
+			# The first paralog has the most XLs (hardcoded as of now).
+			p1 = all_proteins[0]
+
+			alignments_dict[p1] = {}
+			
+			# Path for the protein fasta files.
+			seqa_path = self.fasta_path + f"{protein}/{p1}_human.fasta"
+			# For all the paralogs of a protein.
+			for p2 in all_proteins[1:]:
+				seqb_path = self.fasta_path + f"{protein}/{p2}_human.fasta"
+				outputfile = self.output_path + f"{p1}-{p2}.txt"
+				alignments_dict[p1][p2] = []
+
+				seqa, seqb = self.Needle_alignment(seqa_path, seqb_path, outputfile)
+				
+				prot = []
+				# Obtain all positions to be mapped for non-dominant paralog.
+				[prot.append( pos ) for res in ["1", "2"] for i, pos in enumerate( self.data[f"Residue {res}"] ) if p2 in self.data[f"Protein {res}"][i]]
+				# [original pos, aligned pos]
+				[alignments_dict[p1][p2].append( [position, self.get_aligned_pos( seqa, seqb, position )] ) for position in prot]
+				# if "HDAC" in protein:
+				# 	print(alignments_dict["HDAC1"])
+
+		return alignments_dict
+
+
+	def mapped_XLs( self, alignments_dict ):
+		for i in range( len( self.data["Protein 1"] ) ):
+			if self.exclude and ( self.remove in self.data["Protein 1"][i] or self.remove in self.data["Protein 2"][i] ):
+				continue
+			else:
+				prot1 = self.data["Protein 1"][i].replace( " ", "" )
+				dom_prot1 = self.return_dominant_paralog( prot1 )
+				
+				if prot1 in dom_prot1:
+					self.mapping["Protein 1"].append( prot1 )
+					self.mapping["Residue 1"].append( self.data["Residue 1"][i] )
+				# Consider either of HDAC1/HDAC2 XL and exclude the other.
+				elif self.hdac in prot1 and self.exclude:
+					self.mapping["Protein 1"].append( prot1 )
+					self.mapping["Residue 1"].append( self.data["Residue 1"][i] )			
+				else:
+					self.mapping["Protein 1"].append( dom_prot1 )
+					self.mapping["Residue 1"].append( [pos[1] for pos in alignments_dict[dom_prot1][prot1] if pos[0] == self.data["Residue 1"][i]][0] )
+
+				prot2 = self.data["Protein 2"][i].replace(" ", "")
+				dom_prot2 = self.return_dominant_paralog( prot2 )
+
+				if prot2 in dom_prot2:
+					self.mapping["Protein 2"].append( prot2 )
+					self.mapping["Residue 2"].append( self.data["Residue 2"][i] )
+				# Consider either of HDAC1/HDAC2 XL and exclude the other.
+				elif self.hdac in prot2 and self.exclude:
+					self.mapping["Protein 2"].append( prot2 )
+					self.mapping["Residue 2"].append( self.data["Residue 2"][i] )
+				else:
+					self.mapping["Protein 2"].append( dom_prot2 )
+					self.mapping["Residue 2"].append( [pos[1] for pos in alignments_dict[dom_prot2][prot2] if pos[0] == self.data["Residue 2"][i]][0] )
+
+Align_Paralogs().forward()
+
