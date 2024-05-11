041dca11660b76d35f0c904642c4372a8c6cc405
Kenobi98 <majilakartik123@gmail.com>
2024-05-11 10:03:08 +0530

    Added SUDS3 modeller files

---
+++ input/Sin3A/PDB/model_SUDS3.py
@@ -0,0 +1,20 @@
+# Homology modeling by the automodel class
+from modeller import *              # Load standard Modeller classes
+from modeller.automodel import *    # Load the automodel class
+
+log.level(1)     # request verbose output
+env = environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files
+#env.io.atom_files_directory = ['.', '../atom_files']
+
+a = automodel(env,
+              alnfile  = 'SUDS3_88-172.ali',     # alignment filename
+              knowns   = ('4zqa_A',),              # codes of the templates
+              sequence = 'SUDS3_88-172',
+	      assess_methods=assess.normalized_dope)              # code of the target
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 20                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual homology modeling
+
